4aa85c55c7ced14f74b35a3de7a0ed67441ba768
Ethan Dsouza <156456933+dsouza-ethan@users.noreply.github.com>
2024-04-07 22:45:12 -0700

    Add files via upload

---
+++ (D) EX_JSON_Objects.js
@@ -0,0 +1,9 @@
+
+
+{
+    "_id": ObjectId("60123456789abcdef0123456"),
+    "mode_name": "Cycling",
+    "description": "Using a bicycle for transportation",
+    "carbon_emission_rate_per_mile": 0
+  }
+  
